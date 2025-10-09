0ae7ec638eeb2b3c3ff256ac439ce56e0c3df1c2
Khang Nguyễn <khangnguyenhoangbao@gmail.com>
2025-10-10 00:48:06 +0700

    Init code for backend

---
+++ backend/src/controllers/taskControllers.js
@@ -0,0 +1,59 @@
+import Task from '../models/taskModel.js'
+
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find().sort({ createdAt: -1 })
+    res.status(200).json({ tasks })
+  } catch (error) {
+    console.error("Error retrieving tasks:", error)
+    res.status(500).json({ message: "Error retrieving tasks" })
+  } 
+}
+
+export const createTask = async (req, res) => {
+  try {
+    const { title } = req.body
+    const task = new Task({ title })
+
+    const newTask = await task.save()
+    res.status(201).json({ task: newTask })
+  } catch (error) {
+    console.error("Error creating task:", error)
+    res.status(500).json({ message: "Error creating task" })
+  }
+}
+
+export const updateTask = async (req, res) => {
+  try {
+    const { title, status, completedAt } = req.body
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id, 
+      { 
+        title, 
+        status, 
+        completedAt 
+      }, 
+      { new: true }
+    )
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" })
+    }
+    res.status(200).json(updatedTask)
+  } catch (error) {
+    console.error("Error updating task:", error)
+    res.status(500).json({ message: "Error updating task" })
+  }
+}
+
+export const deleteTask = async (req, res) => {
+  try {
+    const deletedTask = await Task.findByIdAndDelete(req.params.id)
+    if (!deletedTask) {
+      return res.status(404).json({ message: "Task not found" })
+    }
+    res.status(200).json(deletedTask)
+  } catch (error) {
+    console.error("Error deleting task:", error)
+    res.status(500).json({ message: "Error deleting task" })
+  }
+}
